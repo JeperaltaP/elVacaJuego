6da2122d7e5e86f2debfa846be75e8af5555c4b0
JeperaltaP <jeperaltap@eafit.edu.co>
2025-10-28 18:00:07 -0500

    creditspanel

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -4,14 +4,24 @@ using UnityEngine.UI;
 
 public class Buttons : MonoBehaviour
 {
+    public GameObject CreditsPanel;
     public void PlayGame()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void CreditsOn()
+    {
+        CreditsPanel.SetActive(true);
+    }
+
+    public void CreditsOff()
+    {
+        CreditsPanel.SetActive(false);
     }
 
     public void ExitGame()
     {
         Application.Quit();
     }
-
 }   
