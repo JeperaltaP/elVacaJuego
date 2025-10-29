881a0c26934a06836a9ffe64f8175b355e130e7d
JeperaltaP <jeperaltap@eafit.edu.co>
2025-10-28 21:06:21 -0500

    pause menu, configuration in game

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -6,9 +6,15 @@ public class Buttons : MonoBehaviour
 {
     public GameObject CreditsPanel;
     public GameObject ConfigurationPanel;
+    public GameObject PauseMenu;
     public void PlayGame()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void GotoMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 
     public void CreditsOn()
@@ -29,10 +35,25 @@ public class Buttons : MonoBehaviour
     public void ConfigurationOn()
     {
         ConfigurationPanel.SetActive(true);
+        Time.timeScale = 0f;
     }
 
     public void ConfigurationOff()
     {
         ConfigurationPanel.SetActive(false);
+        Time.timeScale = 1f;
     }
+
+    public void PauseOn()
+    {
+        PauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void PauseOff()
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
 }   
